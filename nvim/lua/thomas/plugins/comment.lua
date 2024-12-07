3eb5a235ce89d60a4ac5808be1dd4ed594efbc2b
Thomas Gallaher <ThomasSGallaher@gmail.com>
2024-12-07 02:52:07 -0500

    daily auto commit

--- nvim/lua/thomas/plugins/comment.lua
+++ nvim/lua/thomas/plugins/comment.lua
@@ -1,19 +1,40 @@
 return {
-  "numToStr/Comment.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "JoosepAlviste/nvim-ts-context-commentstring",
-  },
-  config = function()
-    -- import comment plugin safely
-    local comment = require("Comment")
+    "numToStr/Comment.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+        "JoosepAlviste/nvim-ts-context-commentstring",
+    },
+    config = function()
+        -- import comment plugin safely
+        local comment = require("Comment")
 
-    local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
+        local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
 
-    -- enable comment
-    comment.setup({
-      -- for commenting tsx, jsx, svelte, html files
-      pre_hook = ts_context_commentstring.create_pre_hook(),
-    })
-  end,
+        -- enable comment
+        comment.setup({
+            -- for commenting tsx, jsx, svelte, html files
+            padding = true, -- Adds a space between the comment marker and the text
+            sticky = true, -- Keeps the cursor in its position after commenting
+            ignore = "^$", -- lines to ignore when commenting
+            toggler = {
+                line = "gcc", -- Keymap for toggling line comments
+                block = "gbc", -- Keymap for toggling block comments
+            },
+            opleader = {
+                line = "gc", -- Keymap for line comments in operator-pending mode
+                block = "gb", -- Keymap for block comments in operator-pending mode
+            },
+            extra = {
+                above = "gcO", -- Comment on the line above
+                below = "gco", -- Comment on the line below
+                eol = "gcA", -- Comment at the end of the line
+            },
+            mappings = {
+                basic = true, -- Enables `gcc`, `gbc`, etc.
+                extra = true, -- Enables `gcO`, `gco`, `gcA`
+            },
+            post_hook = nil, -- Hook to run after commenting
+            pre_hook = ts_context_commentstring.create_pre_hook(),
+        })
+    end,
 }
--- nvim/lua/thomas/plugins/undotree.lua
+++ nvim/lua/thomas/plugins/undotree.lua
@@ -17,7 +17,7 @@ return {
 
         -- Set keymaps for undotree
         local keymap = vim.keymap -- for conciseness
-        keymap.set("n", "<leader><F5>", vim.cmd.UndotreeToggle)
+        keymap.set("n", "<leader>,", vim.cmd.UndotreeToggle)
         -- Optional settings
         vim.g.undotree_SetFocusWhenToggle = 1 -- Automatically focus the undo tree window when toggled
     end,
